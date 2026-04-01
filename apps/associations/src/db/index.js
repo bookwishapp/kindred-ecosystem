053b02baaefb966824f0a5870d245d338d0ba33b
bookwishapp <terryheath@yahoo.com>
2026-03-31 22:07:59 -0700

    Simplify database path to use local directory for now
    - Use simple local path to get app running
    - Remove Electron dependency from renderer process
    - Proper userData path via IPC will be added later

--- apps/associations/src/db/index.js
+++ apps/associations/src/db/index.js
@@ -1,10 +1,9 @@
 import Database from 'better-sqlite3';
 import path from 'path';
-import { app } from 'electron';
 
-const dbPath = app
-  ? path.join(app.getPath('userData'), 'associations.db')
-  : path.join(process.cwd(), 'associations.db');
+// In renderer process, use a local path for development
+// In production, the path will be passed via IPC from main process
+const dbPath = path.join(process.cwd(), 'associations.db');
 
 const db = new Database(dbPath);
 
